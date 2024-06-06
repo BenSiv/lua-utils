bc06dcf98ee3eaba5f4ef8bca2b8c0e9694873d6
BenSiv <bensiv92@gmail.com>
2024-06-06 09:55:15 +0300

    dealing with required args

--- src/argparse.lua
+++ src/argparse.lua
@@ -1,19 +1,31 @@
 require("utils").using("utils")
+using("dataframes")
 
 -- Define a module table
 local argparse = {}
 
+-- example of arg_string
+-- local arg_string = [[
+--     -d --detach flag string false
+--     -o --output arg string true
+--     -i --iterations arg number false
+-- ]]
+
 local function print_help(expected_args)
     print("Usage: ", arg[0])
+    local help_df = {}
     for _, arg_parsed in pairs(expected_args) do
-        print(
-            "-" .. arg_parsed["short"],
-            "--" .. arg_parsed["long"],
-            "kind: " .. arg_parsed["arg_kind"],
-            "type: " .. arg_parsed["arg_type"],
-            "required: " .. tostring(arg_parsed["is_required"])
-        )
+        local row = {
+            short = "-" .. arg_parsed["short"],
+            long = "--" .. arg_parsed["long"],
+            kind = arg_parsed["arg_kind"],
+            type = arg_parsed["arg_type"],
+            required = tostring(arg_parsed["is_required"])
+        }
+        table.insert(help_df, row)
     end
+    view(help_df)
+    print()
 end
 
 local function add_arg(expected_args, short, long, arg_kind, arg_type, is_required)
@@ -34,7 +46,7 @@ end
 local function def_args(arg_string)
     local expected_args = {}
     for line in arg_string:gmatch("[^\r\n]+") do
-        local short, long, arg_kind, arg_type, is_required = line:match("%s*%-(%w)%s+%-%-(%w+)%s+(%w+)%s+(%w+)%s+(%w+)%s*")
+        local short, long, arg_kind, arg_type, is_required = line:match("%s*%-(%a)%s+%-%-([%a_]+)%s+(%a+)%s+(%a+)%s+(%a+)%s*")
         is_required = is_required == "true"
         expected_args = add_arg(expected_args, short, long, arg_kind, arg_type, is_required)
     end
@@ -79,6 +91,15 @@ local function parse_args(arg, expected_args)
         end
 
         i = i + 1
+    end
+
+    -- Check for required arguments
+    for _, arg_parsed in pairs(expected_args) do
+        if arg_parsed.is_required and result[arg_parsed.long] == nil then
+            print("Missing required argument: --" .. arg_parsed.long .. "\n")
+            print_help(expected_args)
+            return nil
+        end
     end
 
     return result
