34585688448f1435e383058b46afafba41baf6f4
BenSiv <bensiv92@gmail.com>
2024-07-08 13:10:10 +0000

    updating paths module

--- src/paths.lua
+++ src/paths.lua
@@ -18,9 +18,34 @@ local function get_script_dir()
     return script_dir
 end
 
+-- Function to join paths
+local function joinpath(...)
+    local parts = {...}
+    local separator = package.config:sub(1,1)
+    
+    local joined_path = table.concat(parts, separator)
+    
+    if separator == '\\' then
+        joined_path = joined_path:gsub('[\\/]+', '\\')
+    else
+        joined_path = joined_path:gsub('[\\/]+', '/')
+    end
+    
+    return joined_path
+end
+
+-- Function to add relative path to package.path
+local function add_to_path(script_path, relative_path)
+    local script_dir = get_parent_dir(script_path)
+    path_to_add = joinpath(script_dir, relative_path, "?.lua;")
+    package.path = path_to_add .. package.path
+end
+
 paths.get_parent_dir = get_parent_dir
 paths.get_script_path = get_script_path
 paths.get_script_dir = get_script_dir
+paths.joinpath = joinpath
+paths.add_to_path = add_to_path
 
 -- Export the module
 return paths
