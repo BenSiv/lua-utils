64e0e13c516679e990f1ff1dfc62c6e3fc9ef0c6
bensiv <bensiv92@gmail.com>
2024-11-11 16:13:05 +0200

    adding import delimited to database utils

--- src/database.lua
+++ src/database.lua
@@ -1,5 +1,6 @@
 require("utils").using("utils")
 local sqlite = require("sqlite3")
+local readdlm = require("delimited_files").readdlm
 
 -- Define a module table
 local database = {}
@@ -38,8 +39,36 @@ local function local_update(db_path, statement)
     db:close()
 end
 
+local function import_delimited(db_path, file_path, table_name, delimiter)    
+    local db = sqlite.open(db_path)
+    if not db then
+        print("Error opening database")
+        return nil
+    end
+
+    local content = readdlm(file_path, delimiter, true)
+    local col_names = keys(content[1])
+    local col_row = table.concat(col_names, "', '")
+    local insert_statement = string.format("INSERT INTO %s ('%s') VALUES ", table_name, col_row)
+    value_rows = {}
+    local row_values
+    for _, row in pairs(content) do
+        row_values = string.format("('%s')", table.concat(values(row), "', '"))
+        table.insert(value_rows, row_values)
+    end
+    insert_statement = insert_statement .. table.concat(value_rows, ", ") .. ";"
+
+    local _, err = db:exec(insert_statement)
+    if err then
+        print("Error: " .. err)
+    end
+
+    db:close()
+end
+
 database.local_query = local_query
 database.local_update = local_update
+database.import_delimited = import_delimited
 
 -- Export the module
 return database
