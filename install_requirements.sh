e9da880a8e91eea9e95a1661b87e2fc4c5787360
bensiv <bensiv92@gmail.com>
2025-02-23 13:10:06 +0200

    updating requirements

--- install_requirements.sh
+++ install_requirements.sh
@@ -5,7 +5,9 @@ luarocks install --server=https://github.com/acd/lua-yaml.git lua-yaml
 luarocks install --server=https://github.com/moteus/lua-sqlite3.git sqlite3
 luarocks install --server=https://github.com/lunarmodules/luasocket luasocket
 # luarocks install --server=https://github.com/rxi/json.lua.git json
-luarocks install --server=https://github.com/BenSiv/json.lua.git json
+# luarocks install --server=https://github.com/BenSiv/json.lua.git json
+# luarocks install --server=https://github.com/mpx/lua-cjson.git cjson
+luarocks install --server=https://github.com/LuaDist/dkjson.git dkjson
 luarocks install --server=https://github.com/lunarmodules/luasec luasec
 # luarocks install --server=https://github.com/msva/lua-htmlparser.git htmlparser
 # luarocks install https://raw.github.com/sampsyo/lua-sundown/master/lua-sundown-0.1-1.rockspec
